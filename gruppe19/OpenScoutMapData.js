1aa72073e4a9197df60c0d26e8bc3bc6bc71ce9d
openscoutmap <125204917+openscoutmap@users.noreply.github.com>
2023-12-21 15:53:53 +0100

    Added Waypoints
    1/2 done; CaEx RaRo mostly missing

---
+++ gruppe19/OpenScoutMapData.js
@@ -0,0 +1,419 @@
+var geojsonFeature =[
+       {
+        "type": "Feature",
+        "properties": {
+            "name": "Schloss Limberg",
+            "content": ["WiWö SoLa 2023", "WiWö SoLa 2002", "GuSp SoLa 2002","GruppenSoLa 1998","WiWö SoLa 1999", "WiWö+Sp SoLa 1996" ],
+            "state": "STMK",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                15.210505595320939,
+                46.743493865390704
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Techuana",
+            "content": ["GuSp SoLa 2021", "CaEx SoLa 2021", "CaEx SoLa 2009", "GruppenSoLa 1993"],
+            "state": "KTN",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                13.96861113034732,
+                46.58015156193912
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Regensburg (Burg Regeldorff)",
+            "content": ["GuSp SoLa 2023","Gu SoLa 2012" ],
+            "state": "BAY",
+            "country": "GER",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                12.107475712658282,
+                49.092658704723796
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "St. Johann i. Pongau (Ferienhaus Waldhof)",
+            "content": ["WiWö SoLa 2019","WiWö SoLa 2017", "WiWö SoLa 2009", "WiWö SoLa 2004" ],
+            "state": "SB",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                13.191870699262424,
+                47.32936171667742, 
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Werfenweng (Leitenmühle)",
+            "content": ["GuSp SoLa 2019"],
+            "state": "SB",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                13.246268268085126,
+                47.460820169665986
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Oberpullendorf (Lagerplatz Kaisereiche)",
+            "content": ["RaRo SoLa 2020","GuSp SoLa 2017" ],
+            "state": "NOE",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [ 
+                16.520660774945856,
+                47.49780956318315
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Pfadfinderdorf Zellhof",
+            "content": ["GuSp SoLa 2013","GuSp SoLa 1999" ],
+            "state": "SB",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                13.092497883765848,
+                47.98486703103364
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Bangs (Witzy Camp)",
+            "content": ["GuSp SoLa 2015","Sp SoLa 2012", "SpEx SoLa 2004", "RaRo SoLa 2004", "CaEx SoLa 1997", "RaRo SoLa 1997" ],
+            "state": "VAB",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                9.535807673706158,
+                47.27402233248537
+            ]
+        }
+    },{
+        "type": "Feature",
+        "properties": {
+            "name": "Bludenz",
+            "content": ["RaRo SoLa 2021"],
+            "state": "VAB",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                9.83406188187439, 
+                47.14699465241122
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Mühlbach am Hochkönig(Mitterberghaus)",
+            "content": ["Gruppen SoLa 2022"],
+            "state": "SB",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                13.116994545995546,
+                47.40146389962504
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "St. Georgen (Scout Camp Austria)",
+            "content": ["Gruppen SoLa 2011", "Gruppen SoLa 2006", "Gruppen SoLa 2001"],
+            "state": "OOE",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                13.500233999106516,
+                47.95407389802147
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Pfadfinderzentrum Leibnitz",
+            "content": ["Gruppen SoLa 2018", "Gruppen SoLa 2008", "Gruppen SoLa 2003", "SpEx SoLa 2000"],
+            "state": "STMK",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                15.536797177800597,
+                46.77213524198883
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Vienna 2000 (Flugfeld Aspern)",
+            "content": ["GuCa SoLa 2000"],
+            "state": "VIE",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                16.504034107660814,
+                48.21662105131403 
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Edling (Edlinger Hof)",
+            "content": ["WiWö SoLa 2015","WiWö SoLa 2012" ],
+            "state": "KTN",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                14.704068095429744,
+                46.62298405055621
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Sandgrube (Wetzelsdorf)",
+            "content": ["CaEx SoLa 2016"],
+            "state": "STMK",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                15.874079693455409,
+                46.997724102132885
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "TPZ Igels",
+            "content": ["Gruppen SoLa 2014", "WiWö SoLa 1997", "GuSP SoLa 1997", "Gruppen SoLa 1995"],
+            "state": "TIR",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                11.431793802816657,
+                47.22295966423392
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Scout Park Cervignano del Friuli",
+            "content": ["RaRo SoLa 2023 (Aufenthalt)","CaEx SoLa 2015"],
+            "state": "-",
+            "country": "ITA",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                13.32839315637918, 
+                45.812209921051014
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Zeltlagerplatz Naturns",
+            "content": ["RaRo SoLa 2016 (Aufenthalt)"],
+            "state": "-",
+            "country": "ITA",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                11.003190775318707, 
+                46.638803474282305
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "urSprung (Laxenburg)",
+            "content": ["GuSp SoLa 2010", "Ca SoLa 2010", "RaRo SoLa 2010"],
+            "state": "NOE",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                16.368598079162407,
+                48.05633371436143
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Hard",
+            "content": ["CaEx SoLa 2012"],
+            "state": "NOE",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                9.682745226426816,
+                47.48746468749834            
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Judendorf-Straßengel(Lagerplatz Graz 3)",
+            "content": ["GuSp SoLa 2016"],
+            "state": "STMK",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                15.342215393479053,
+                47.11239822185421
+           
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Kirchschlag in der Buckligen Welt (Lilienhof)",
+            "content": ["WiWö SoLa 2016"],
+            "state": "NOE",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                16.29470742454947,
+                47.53810484319862
+            ]
+        }
+    },
+    {
+        "type": "Feature",
+        "properties": {
+            "name": "Dornbirn",
+            "content": ["RaRo SoLa 2013"],
+            "state": "VAB",
+            "country": "AUT",
+            "cathegory": "mixed",
+            "id": 1
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                9.740473537724219,
+                47.408340712409846
+            ]
+        }
+    }
+
+
+
+];
